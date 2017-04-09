bbfec5750a69625a73c24395b9612ff0478d3dd0
Wei-Cheng Pan <legnaleurc@gmail.com>
2017-04-09 11:17:49 +0900

    _ file m

--- src/sites/file/mirrorupload.net.js
+++ src/sites/file/mirrorupload.net.js
@@ -1,15 +1,13 @@
-$.register({
+_.register({
   rule: {
     host: /^www.mirrorupload.net$/,
   },
-  ready: function () {
-    'use strict';
-
+  async ready () {
     // Find the form leading to the mirrors page
-    var accessForm = $('form[name=form_upload]');
+    const accessForm = $('form[name=form_upload]');
 
     // We need to append this field or this doesn't redirect correctly
-    var accessInput = document.createElement('input');
+    const accessInput = document.createElement('input');
     accessInput.type = 'hidden';
     accessInput.name = 'access';
     accessInput.value = Math.random();
