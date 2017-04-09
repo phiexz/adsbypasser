4d84620d8f4d55585d1ef3bf36817ff5416a7555
Wei-Cheng Pan <legnaleurc@gmail.com>
2017-04-09 11:58:26 +0900

    _ image e

--- src/sites/image/empireload.com.js
+++ src/sites/image/empireload.com.js
@@ -1,12 +1,10 @@
-$.register({
+_.register({
   rule: {
     host: /^(www\.)?empireload\.com$/,
     path: /^(\/images(\/files\/\w)?)\/.\.php$/,
     query: /^\?link=(.+)$/,
   },
-  start: function (m) {
-    'use strict';
-
-    $.openImage(m.path[1] + '/link/' + m.query[1]);
+  async start (m) {
+    await $.openImage(m.path[1] + '/link/' + m.query[1]);
   },
 });
