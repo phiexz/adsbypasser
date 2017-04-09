ca92452fb18d9b56cd203fd2d7f9b467bd02449b
Wei-Cheng Pan <legnaleurc@gmail.com>
2017-04-09 12:01:54 +0900

    _ image f

--- src/sites/image/freakimage.com.js
+++ src/sites/image/freakimage.com.js
@@ -1,12 +1,10 @@
-$.register({
+_.register({
   rule: {
     host: /^freakimage\.com|www\.hostpic\.org$/,
     path: /^\/view\.php$/,
     query: /^\?filename=([^&]+)/,
   },
-  start: function (m) {
-    'use strict';
-
-    $.openImage('/images/' + m.query[1]);
+  async start (m) {
+    await $.openImage('/images/' + m.query[1]);
   },
 });
