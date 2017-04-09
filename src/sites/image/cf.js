a1cb6dd5ccc448846095561229420aa6057face0
Wei-Cheng Pan <legnaleurc@gmail.com>
2017-04-09 11:35:54 +0900

    _ image c

--- src/sites/image/cf.js
+++ src/sites/image/cf.js
@@ -1,11 +1,9 @@
-$.register({
+_.register({
   rule: {
     host: /^www\.x45x\.info|(imadul|mypixxx\.lonestarnaughtygirls)\.com|ghanaimages\.co|imgurban\.info|d69\.in$/,
     query: /\?p[mt]=(.+)/,
   },
-  start: function (m) {
-    'use strict';
-
-    $.openImage('/?di=' + m.query[1]);
+  async start (m) {
+    await $.openImage('/?di=' + m.query[1]);
   },
 });
