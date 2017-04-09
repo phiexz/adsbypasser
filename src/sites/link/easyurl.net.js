b7e1005276577508d6c88e34a94c86bd95096cd6
Wei-Cheng Pan <legnaleurc@gmail.com>
2017-04-09 17:12:20 +0900

    _ link e

--- src/sites/link/easyurl.net.js
+++ src/sites/link/easyurl.net.js
@@ -1,12 +1,9 @@
-$.register({
+_.register({
   rule: {
     host: /easyurl\.net|(atu|clickthru|redirects|readthis)\.ca|goshrink\.com$/,
   },
-  ready: function () {
-    'use strict';
-
-    var f = $('frame[name=main]');
-
-    $.openLink(f.src);
+  async ready () {
+    const f = $('frame[name=main]');
+    await $.openLink(f.src);
   },
 });
--- src/sites/link/empireload.com.js
+++ src/sites/link/empireload.com.js
@@ -1,10 +1,10 @@
-$.register({
+_.register({
   rule: {
     host: /empireload\.com$/,
     path: /^\/plugin\.php$/,
     query: /^\?id=linkout&url=([^&]+)$/,
   },
-  start: function (m) {
-    $.openLink(m.query[1]);
+  async start (m) {
+    await $.openLink(m.query[1]);
   },
 });
